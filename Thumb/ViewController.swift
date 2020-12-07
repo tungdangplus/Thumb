33e757e984e2a4e6eb93048a19c3de62d6b2bce0
Tung Dang <tungdangplus@gmail.com>
2020-12-07 11:10:27 -0800

    Initial Commit

---
+++ Thumb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thumb
+//
+//  Created by TungDang on 07/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
